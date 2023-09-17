6a65f910e69e3b4e8fe5f40be6efe7adc3e0c51f
CharlieMV <143232951+CharlieMV@users.noreply.github.com>
2023-09-16 17:20:02 -0700

    Add files via upload

--- LetterCount.java
+++ LetterCount.java
@@ -5,6 +5,8 @@
  *	@author Charles Chang	
  *	@since September 14
  */
+
+ 
 public class LetterCount {
 	
 	// Fields go here, all must be private
@@ -28,13 +30,11 @@ public class LetterCount {
 	 *	- prints the histogram of the letter count
 	 */
 	public void run(String[] args) {
-		private int charCount[];
+		int[] anArray;
 		charCount = new int[36];
-		private char charList[];
-		charList = new char['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-		'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-		private boolean found = false;
-		private int search = 0;
+		char[] charList = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+		boolean found = false;
+		int search = 0;
 		String fileName = args[0];
 		File newFile = new File("cities.txt");
 		Scanner newReader = new Scanner(newFile);
@@ -44,8 +44,13 @@ public class LetterCount {
 				search = 0;
 				found = false;
 				while (found == false) {
-					if (charAt()
+					if (charAt() == charList[search]){
+						charCount[search] ++;
+						found = true;
+					}
+					search++;
 				}
+				data = data.substring(1, data.length());
 			}
 		}
 	}
